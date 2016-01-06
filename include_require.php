f679740b9ac80c6258754535dd3613334a75abaf
Trevor Roman <troman360@gmail.com>
2016-01-05 22:27:26 -0500

    finished include/require

---
+++ include_require.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	
+<head>
+	<title>PHP Include / Require</title>
+</head>
+
+<body>
+
+	<?php 
+		include("table.html");
+		require("form.html");
+	?>
+
+</body>
+
+</html>
