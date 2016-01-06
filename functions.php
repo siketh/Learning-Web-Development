0dbc85295eda6c2ecb31435d19fd2a03d9b88733
Trevor Roman <troman360@gmail.com>
2016-01-05 22:17:56 -0500

    finished functions

--- functions.php
+++ functions.php
@@ -6,10 +6,21 @@
 </head>
 
 <body>
-	
+
 	<?php 
 
-	
+		function hello($name)
+		{
+			echo "Hello world!<br>";
+
+			if(strcmp($name, "Trevor") == 0)
+				echo "Hello " . $name . "!<br>";
+			else
+				echo "Hello stranger!<br>";
+		}
+
+		hello("Trevor");
+		hello("Mark");
 
 	?>
 
